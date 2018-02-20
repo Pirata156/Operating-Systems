6024480e9c7de9645f1692a554d1fc30c2e95385
Pirata <Pirata156@users.noreply.github.com>
2018-02-20 20:36:43 +0000

    fim readline version 1

--- Guiao01/readln_v1.c
+++ Guiao01/readln_v1.c
@@ -1,35 +1,37 @@
+#include "readln_v1.h"
 #include <stdlib.h>
-#include <unistd.h>
-#include <fcntl.h>
-#include <stdio.h>
 
-
-ssize_t readln(int fildes, void *buf, size_t nbyte)
+ssize_t readln_v1(int fildes, void *buf, size_t nbyte)
 {
-	char* buff = buf;
-	size_t i = -1;
-	ssize_t n;
-	do {
-		i++;
-		n = read(fildes,buff+i,1);
-	} while((i < nbyte) && (n > 0) && (buff[i] != '\n'));
-	n = i;
-	exit(n);
-}
+	size_t i = 0;
+	ssize_t n = 1;
+	char c = ' ';
+	
+	/* Enquanto está a ler algo mas que seja menos de nbyte caracteres, e não seja o '\n' */
+	while((i < nbyte) && (n > 0) && (c != '\n')) {
+		//Lê um caractere
+		n = read(fildes, &c, 1);
+		// Se não for o '\n' adiciona-o ao buffer
+		if(n && (c != '\n')) {
+			((char*) buf)[i] = c;
+			i++;
+		}
+	}
 
-int main(int argc,char** argv)
-{
-	char buffer[100];
-	int fd,n;
-	
-	fd = open("texto.txt", O_RDONLY);
-	do {
-		n = readln(fd, buffer, 100);
-	
-		printf("A linha que li foi [%s]\n",buffer);
-	
-	} while (n > 0);
-	close(fd);
-	
-	return 0;
+	// Adição de EOF == 0 com verificação no caso de chegar ao limite de leitura (N);
+	if(i < nbyte)
+		((char*) buf)[i] = 0;
+	else {
+		// passou o limite (i == 100). buf[99] = EOF.
+		i--;
+		((char*) buf)[i] = 0;
+	}
+
+	// se deu erro na leitura retorna esse mesmo erro
+	if(n < 0)
+		return n;
+	// no caso de apanhar a linha só com o '\n'
+	if((n == 0) && (i == 0))
+		return (-1);
+	return i;
 }
